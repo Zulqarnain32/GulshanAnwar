0819091c748bf2b77a75323cf42faa8ee7e97b40
Zulqarnian Chohan <zulqarnainchohan250@gmail.com>
2024-05-27 22:06:12 +0500

    Update PlayersData.jsx

--- src/apiPlayers/PlayersData.jsx
+++ src/apiPlayers/PlayersData.jsx
@@ -3,12 +3,20 @@ const PlayersData = [
         id:1,
         name:"Zeeshan",
         age:23,
-        position:"Forward",
+        p{osition:"Forward",
         image:"/assets/zeeshan.jpg"
     },
     {
         id:2,
         name:"Umair",
+        age:18,
+        position:"Forward",
+        image:"/assets/umair.jpg"
+
+    },
+        {
+        id:2,
+        name:"Zuraiz",
         age:18,
         position:"Forward",
         image:"/assets/umair.jpg"
